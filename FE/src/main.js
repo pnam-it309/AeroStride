f64fb49ff1e17cba0914ef9db262a83bf15427d4
ADMIN <phitrang082006@gmail.com>
2026-04-03 12:39:42 +0700

    Done FE phiếu giảm giá và đợt giảm giá

--- FE/src/main.js
+++ FE/src/main.js
@@ -3,6 +3,7 @@ import '@phosphor-icons/web/bold' // Import Phosphor Bold icons via NPM
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { setupValidation } from '@/utils/validation'
+import 'element-plus/dist/index.css' // QUAN TRỌNG: Dòng này để có giao diện
 
 import App from './App.vue'
 import router from './router'
