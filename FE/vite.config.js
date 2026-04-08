6f1c7478951280d81fa918accd24d8ce87fa2d56
pnam-it309 <nickhunter3009@gmail.com>
2026-04-08 21:02:02 +0700

    them /aero

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
             styles: { configFile: 'src/scss/variables.scss' }
         })
     ],
+    base: '/AeroStride/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
