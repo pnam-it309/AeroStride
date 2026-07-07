7196b5f44a79086aa32301524cd1f30bf57c26ba
pnamit-309 <nickhunter3009@gmail.com>
2026-07-07 20:44:08 +0700

    chore: remove mongodb and add intellij run config

--- BE/src/main/java/com/example/be/BeApplication.java
+++ BE/src/main/java/com/example/be/BeApplication.java
@@ -4,10 +4,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude = {MongoRepositoriesAutoConfiguration.class})
+@SpringBootApplication
 @EnableAsync
 public class BeApplication {
 
